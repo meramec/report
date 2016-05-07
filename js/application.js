96ec9890950cacfd2bee2a1b0511688b49a7cd26
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2016-05-07 18:17:51 -0500

    Deploy to Github Pages: a32ad1d088bf85e766ef7e9c602bfb1ba3ce4883

--- js/application.js
+++ js/application.js
@@ -1,4 +1,24 @@
+// /home/ryan/github/meramec/report/app/js/reportGenerator/reportTitle.js
+(function() {
+  angular.module('report.generator').directive('reportTitle', reportTitle);
+
+  function reportTitle() {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'templates/reportGenerator/reportTitle.html'
+    };
+  }
+
+})();
+
 // /home/ryan/github/meramec/report/app/js/app.js
 (function() {
+  var app = angular.module('ReportGenerator', ['report.generator']);
+})();
+
+// /home/ryan/github/meramec/report/app/js/reportGenerator.js
+(function() {
+  angular.module('report.generator', []);
 })();
 
