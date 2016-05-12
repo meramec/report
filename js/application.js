b2447f5dbc82dcfa104660a224ca4396b17b3c39
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2016-05-12 00:00:42 -0500

    Deploy to Github Pages: 2b9f710df1be38ed27e7acababb83c0c40347b0d

--- js/application.js
+++ js/application.js
@@ -3,6 +3,11 @@
   angular.module('google.api', []);
 })();
 
+// /home/ryan/github/meramec/report/app/js/picker.js
+(function() {
+  angular.module('picker', ['google.api']);
+})();
+
 // /home/ryan/github/meramec/report/app/js/app.js
 (function() {
   var app = angular.module('ReportGenerator', ['report.generator']);
@@ -10,7 +15,7 @@
 
 // /home/ryan/github/meramec/report/app/js/reportGenerator.js
 (function() {
-  angular.module('report.generator', ['google.api']);
+  angular.module('report.generator', ['picker']);
 })();
 
 // /home/ryan/github/meramec/report/app/js/googleApi/sheets.js
@@ -32,7 +37,7 @@
 
 // /home/ryan/github/meramec/report/app/js/googleApi/auth.js
 (function() {
-  angular.module('google.api').service('auth', ['$http', auth]);
+  angular.module('google.api').service('auth', ['client', auth]);
 
   var clientId = '215619993678-kdcmgv8u79r9vdmti2m3ldjuvqgagnb7.apps.googleusercontent.com';
   var scopes = [
@@ -41,69 +46,34 @@
     'https://spreadsheets.google.com/feeds'
   ];
 
-  var onAuthorize;
-  var loaded;
-  window.onGoogleLoaded = function() {
-    loaded = true;
-    if(onAuthorize)
-      onAuthorize();
-  };
-
-  function auth($http) {
-
+  function auth(client) {
     this.authorize = function(callback) {
-      var options = {
-        client_id: clientId,
-        scope: scopes.join(' '),
-        immediate: true
-      };
-  
-      var get = function() {
-        getAuthorization(options, callback);
-      };
-
-      if(loaded)
-        get();
-      else
-        onAuthorize = get;
+      client.authorization(clientId, scopes).authorize(callback);
     };
-
-    this.getToken = function() {
-      return gapi.auth.getToken().access_token;
-    };
-
-    function getAuthorization(options, callback) {
-      gapi.auth.authorize(options, function(result) {
-        if(result && ! result.error) {
-          callback();
-        } else if(options.immediate) {
-          options.immediate = false;
-          getAuthorization(options, callback);
-        }
-      });
-    }
   }
 })();
 
 // /home/ryan/github/meramec/report/app/js/googleApi/drive.js
 (function() {
-  angular.module('google.api').service('drive', ['client', drive]);
+  angular.module('google.api').service('drive', ['client', 'latch',  drive]);
 
-  function drive(client) {
+  function drive(client, latch) {
     var lib = client.load('drive', 'v3');
 
     this.buildTree = function(drive, types) {
       var notify = new Notify();
       lib.start(function() {
-        new BuildTree(drive, types, notify).begin();
+        new BuildTree(drive, types, latch, notify).begin();
       });
 
       return notify;
     }; 
   }
 
-  function BuildTree(drive, types, notify) {
+  function BuildTree(drive, types, latch, notify) {
     var self = this;
+
+    var onRootLoaded = latch.create();
 
     var fields = 'id,mimeType,name,parents,size,ownedByMe,owners(displayName)';
     var opts = {
@@ -129,9 +99,23 @@
     function getRootFolder() {
       var request = gapi.client.drive.files.get({fileId: 'root', fields: fields});
       request.execute(function(response) {
-        drive.folders[0] = update(response.result.id, response.result);
-        notify.onNotifyChange();
+        drive.folders.unshift(update(response.result.id, response.result));
+        onRootLoaded.ready();
       });
+    }
+
+    function getSharedFolder() {
+      if(files['shared']) {
+        return files['shared'];
+      } else  {
+        var shared = acquire('shared');
+        update('shared', { name: 'Shared With Me' });
+        drive.folders.push(shared);
+
+        setNotEmpty(shared);
+
+        return shared;
+      }
     }
 
     function getFiles(opts) {
@@ -146,37 +130,42 @@
           _.each(response.files, function(file) {
             if(file.ownedByMe) {
               if(file.mimeType === 'application/vnd.google-apps.folder') {
-                file = update(file.id, file);
+                addFolderToParents(file);
+              } else {
+                addFileToParents(file);
               }
-
-              _.each(file.parents, function(id) {
-                var p = acquire(id);
-                if(file.mimeType === 'application/vnd.google-apps.folder') {
-                  p.folders.push(file);
-
-                  if(! file.empty)
-                    setNotEmpty(p);   
-                } else {
-                  p.files.push(file);
-                  setNotEmpty(p);
-                }
-              });
             } else {
-              var shared = acquire('shared');
-              update('shared', { name: 'Shared With Me' });
-              drive.folders[1] = shared;
-
-              shared.files.push(file);
-              setNotEmpty(shared);
+              getSharedFolder().files.push(file);
             }
           });
         }
+
         notify.onNotifyChange();
 
-        if(! response.nextPageToken)
-          notify.onNotifyComplete();
+        if(! response.nextPageToken) {
+          onRootLoaded.wait(function() { 
+            notify.onNotifyComplete();
+          });
+        } 
       });
+    }
 
+    function addFolderToParents(folder) {
+      folder = update(folder.id, folder);
+      _.each(folder.parents, function(id) {
+        var p = acquire(id);
+        p.folders.push(folder);
+        if(! folder.empty)
+          setNotEmpty(p);
+      });
+    }
+
+    function addFileToParents(file) {
+      _.each(file.parents, function(id) {
+        var p = acquire(id);
+        p.files.push(file);
+        setNotEmpty(p);
+      });
     }
 
     function update(id, file) {
@@ -232,122 +221,6 @@
 
 })();
 
-// /home/ryan/github/meramec/report/app/js/googleApi/picker.js
-(function() {
-  angular.module('google.api').directive('picker', picker);
-  angular.module('google.api').controller('PickerController', ['$scope', '$timeout', PickerController]);
-
-  function picker() {
-    return {
-      restrict: 'E',
-      replace: true,
-      templateUrl: 'templates/googleApi/picker.html',
-      controller: 'PickerController',
-      scope: true
-    };
-  }
-
-  function PickerController($scope, $timeout) {
-    $scope.showDrive = true;
-
-    $scope.$on('choose-file', function() {
-      $scope.openModal = true;
-      $timeout(function() {
-        $scope.$digest();
-      });
-    });
-    $scope.dismiss = function() {
-      $scope.openModal = false;
-      $timeout(function() {
-        $scope.$digest();
-      });
-    };
-  }
-
-})();
-
-
-// /home/ryan/github/meramec/report/app/js/googleApi/folder.js
-(function() {
-  angular.module('google.api').directive('folder', folder);
-  angular.module('google.api').controller('FolderController', ['$scope', FolderController]);
-
-  function folder() {
-    return {
-      restrict: 'E',
-      replace: true,
-      templateUrl: 'templates/googleApi/folder.html',
-      controller: 'FolderController',
-    };
-  }
-
-  function FolderController($scope) {
-    $scope.folder.open = false;
-
-    $scope.onClick = function(e) {
-      e.stopPropagation();
-
-      $scope.folder.open = ! $scope.folder.open;
-    };
-  }
-})();
-
-// /home/ryan/github/meramec/report/app/js/googleApi/file.js
-(function() {
-  angular.module('google.api').directive('file', file);
-  angular.module('google.api').controller('FileController', ['$scope', 'selectAction', FileController]);
-
-  function file() {
-    return {
-      restrict: 'E',
-      replace: true,
-      templateUrl: 'templates/googleApi/file.html',
-      controller: 'FileController',
-    };
-  }
-
-  function FileController($scope, selectAction) {
-    $scope.onClick = function(e) {
-      e.stopPropagation();
-
-      selectAction.openSheet($scope, $scope.file);
-    };
-  }
-})();
-
-// /home/ryan/github/meramec/report/app/js/googleApi/browseDrive.js
-(function() {
-  angular.module('google.api').directive('browseDrive', browseDrive);
-  angular.module('google.api').controller('BrowseDriveController', ['$scope', 'drive', 'me', BrowseDriveController]);
-
-  function browseDrive() {
-    return {
-      restrict: 'E',
-      replace: true,
-      templateUrl: 'templates/googleApi/browseDrive.html',
-      controller: 'BrowseDriveController',
-      scope: {}
-    };
-  }
-
-  function BrowseDriveController($scope, drive) {
-
-    $scope.drive = {
-      folders: []
-    };
-
-    drive.buildTree($scope.drive, ['application/vnd.google-apps.spreadsheet'])
-      .onChange(function() {
-        if($scope.drive.folders[0])
-          $scope.drive.folders[0].open = true;
-
-        $scope.$digest();
-      }).onComplete(function() {
-
-      });
-  }
-})();
-
 // /home/ryan/github/meramec/report/app/js/googleApi/me.js
 (function() {
   angular.module('google.api').service('me', ['client', me]);
@@ -355,46 +228,126 @@
   function me(client) {
     var lib = client.load('plus', 'v1');
 
-    this.load = function(me) {
+    this.load = function(callback) {
       lib.start(function() {
-        var fields = 'displayName, emails, id, image(url)';
+        var fields = 'displayName,id,image(url)';
         var request = gapi.client.plus.people.get({userId: 'me', fields: fields});
 
         request.execute(function(response) {
-          console.log(JSON.stringify(response));
+          callback(response.result); 
         });
       });
     };
   }
 })();
 
-// /home/ryan/github/meramec/report/app/js/googleApi/client.js
+// /home/ryan/github/meramec/report/app/js/googleApi/latch.js
 (function() {
-  angular.module('google.api').factory('client', client);
+  angular.module('google.api').factory('latch', latch);
 
-  function client() {
-    var loaded;
+  function latch() {
     return {
-      load: function(lib, version) {
-        var run;
-        gapi.client.load(lib, version, function() {
-          loaded = true;
-
-          if(run)
-            run();
-        });
-
-        return {
-          start: function(fn) {
-            if(loaded)
-              fn();
-            else
-              run = fn;
-          }
-        };
+      create: function() {
+        return new Latch();
       }
     };
+  }
+
+  function Latch() {
+    var ready;
+
+    var calls = [];
+
+    this.ready = function() {
+      ready = true;
+
+      while(calls.length > 0) {
+        var call = calls.shift();
+        call();
+      }
+    };
+
+    this.wait = function(fn) {
+      if(ready) {
+        fn();
+      } else {
+        calls.push(fn);
+      }
+    };
+  }
+})();
+
+// /home/ryan/github/meramec/report/app/js/googleApi/client.js
+(function() {
+  angular.module('google.api').service('client', ['$window', '$document', 'latch', client]);
+
+  var clientLoaded;
+
+  function client($window, $document, latch) {
+
+    clientLoaded = latch.create();
+
+    $window.onClientLoaded = function() {
+      clientLoaded.ready();
+    };
+
+    loadClient();
+
+    this.authorization = function(clientId, scopes) {
+      return new Authorization(clientId, scopes);
+    };
+
+    this.authToken = function() {
+      return gapi.auth.getToken().access_token
+    };
+
+    this.load = function(lib, version) {
+      return new Library(lib, version);
+    };
+
+    function loadClient() {
+      var clientjs = $document[0].createElement('script');
+      clientjs.src = 'https://apis.google.com/js/client.js?onload=onClientLoaded';
+      $document[0].body.appendChild(clientjs);
+    }
+
+    function Authorization(clientId, scopes) {
+      var options = {
+        client_id: clientId,
+        scope: scopes.join(' '),
+        immediate: true
+      };
+
+      this.authorize = function(callback) {
+        clientLoaded.wait(function() {
+          gapi.auth.authorize(options, function(result) {
+            if(result && ! result.error) {
+              callback();
+            } else if(options.immediate) {
+              options.immediate = false;
+              this.authorize(options, callback);
+            }
+          });
+        });
+      };
+    }
+
+    function Library(lib, version) {
+      var libraryLoaded = latch.create();
+
+      clientLoaded.wait(function() {
+        gapi.client.load(lib, version, function() {
+          libraryLoaded.ready();
+        });
+      });
+
+      this.start = function(fn) {
+        libraryLoaded.wait(fn);
+      };
+    }
   };
+
+
 })();
 
 // /home/ryan/github/meramec/report/app/js/reportGenerator/appTitle.js
@@ -414,7 +367,7 @@
   function AppTitleController($scope, $window) {
 
     function updateTitle() {
-      $window.document.title = $scope.pageTitle + ' | ' + $scope.pageSubtitle;
+      $window.document.title = $scope.report.title + ' | ' + $scope.report.subtitle;
     }
 
     updateTitle();
@@ -457,8 +410,33 @@
     auth.authorize(onReady);
 
     function onReady() {
+      $scope.$broadcast('authenticated');
       $scope.$broadcast('choose-file');
     }
+  }
+})();
+
+// /home/ryan/github/meramec/report/app/js/reportGenerator/user.js
+(function() {
+  angular.module('report.generator').directive('user', user);
+  angular.module('report.generator').controller('UserController', ['$scope', 'me', UserController]);
+
+  function user() {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'templates/reportGenerator/user.html',
+      controller: 'UserController'
+    };
+  }
+
+  function UserController($scope, me) {
+    $scope.$on('authenticated', function() {
+      me.load(function(user) {
+        $scope.user = user;
+        $scope.$digest();
+      });
+    });
   }
 })();
 
@@ -482,4 +460,116 @@
     };
   }
 })();
+
+// /home/ryan/github/meramec/report/app/js/picker/folder.js
+(function() {
+  angular.module('picker').directive('folder', folder);
+  angular.module('picker').controller('FolderController', ['$scope', FolderController]);
+
+  function folder() {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'templates/picker/folder.html',
+      controller: 'FolderController',
+    };
+  }
+
+  function FolderController($scope) {
+    $scope.onClick = function(e) {
+      e.stopPropagation();
+
+      $scope.folder.open = ! $scope.folder.open;
+    };
+  }
+})();
+
+// /home/ryan/github/meramec/report/app/js/picker/file.js
+(function() {
+  angular.module('picker').directive('file', file);
+  angular.module('picker').controller('FileController', ['$scope', FileController]);
+
+  function file() {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'templates/picker/file.html',
+      controller: 'FileController',
+    };
+  }
+
+  function FileController($scope, selectAction) {
+    $scope.onClick = function(e) {
+      e.stopPropagation();
+    };
+  }
+})();
+
+// /home/ryan/github/meramec/report/app/js/picker/browseDrive.js
+(function() {
+  angular.module('picker').directive('browseDrive', browseDrive);
+  angular.module('picker').controller('BrowseDriveController', ['$scope', 'drive', BrowseDriveController]);
+
+  function browseDrive() {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'templates/picker/browseDrive.html',
+      controller: 'BrowseDriveController',
+      scope: {}
+    };
+  }
+
+  function BrowseDriveController($scope, drive) {
+
+    $scope.drive = {
+      folders: []
+    };
+
+    drive.buildTree($scope.drive, ['application/vnd.google-apps.spreadsheet'])
+      .onChange(function() {
+        $scope.$digest();
+      }).onComplete(function() {
+        if($scope.drive.folders[0]) {
+          $scope.drive.folders[0].open = true;
+          $scope.$digest();
+        }
+      });
+  }
+})();
+
+// /home/ryan/github/meramec/report/app/js/picker/pickerModal.js
+(function() {
+  angular.module('picker').directive('pickerModal', pickerModal);
+  angular.module('picker').controller('PickerModalController', ['$scope', '$timeout', PickerModalController]);
+
+  function pickerModal() {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'templates/picker/pickerModal.html',
+      controller: 'PickerModalController',
+      scope: true
+    };
+  }
+
+  function PickerModalController($scope, $timeout) {
+    $scope.showDrive = true;
+
+    $scope.$on('choose-file', function() {
+      $scope.openModal = true;
+      $timeout(function() {
+        $scope.$digest();
+      });
+    });
+    $scope.dismiss = function() {
+      $scope.openModal = false;
+      $timeout(function() {
+        $scope.$digest();
+      });
+    };
+  }
+
+})();
+
 
