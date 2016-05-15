4377593c60e8bd8a8a75e3b52caee43345e3f21d
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2016-05-15 18:01:33 -0500

    WIP styling and event handling for details

--- app/js/reportGenerator/details.js
+++ app/js/reportGenerator/details.js
@@ -1,6 +1,6 @@
 (function() {
   angular.module('report.generator').directive('details', details);
-  angular.module('report.generator').controller('DetailsController', ['$scope', DetailsController]);
+  angular.module('report.generator').controller('DetailsController', ['$scope', '$document', DetailsController]);
 
   function details() {
     return {
@@ -11,7 +11,7 @@
     };
   }
 
-  function DetailsController($scope) {
+  function DetailsController($scope, $document) {
     $scope.worksheet.details =  _.map(_.tail($scope.worksheet.headers, 1), function(name, j) {
       var entries = _.filter($scope.worksheet.data, function(row) {
         return row[j+1];
@@ -20,8 +20,21 @@
       return {name: name, value: entries.length};
     });
 
-    $scope.onClick = function() {
+    $scope.onClick = function(e) {
+      e.stopPropagation();
       $scope.showDetails = ! $scope.showDetails;
+
+      function handleClick() {
+        $scope.showDetails = false;
+        $scope.$digest();
+      }
+
+      if($scope.showDetails) {
+        $document.bind('click', handleClick);
+      
+      } else {
+        $document.unbind('click', handleClick);
+      }
     };
   }
 })();
